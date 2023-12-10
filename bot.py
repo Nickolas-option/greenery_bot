58ecf063881f1c1801d3c47a92064dde96a1de93
yaeooa <145396563+yaeooa@users.noreply.github.com>
2023-12-10 23:47:09 +0300

    Обновление бота под облачное решение

--- bot.py
+++ bot.py
@@ -1,28 +1,29 @@
+import asyncio
+import json
 import logging
+import os
 import random
-
-from aiogram import Bot, Dispatcher, executor, types
+ 
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-
-API_TOKEN = '6758375137:AAFosYicJRZd5C6AJYlzojaoxHAdpEOlTNM'
-logging.basicConfig(level=logging.INFO)
+ 
+# Logger initialization and logging level setting
+log = logging.getLogger(__name__)
+log.setLevel(os.environ.get('LOGGING_LEVEL', 'INFO').upper())
+ 
+API_TOKEN = 'TOKEN'  
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-
-
-class TrackingState(StatesGroup):
-    waiting_for_stock_symbol = State()
-    waiting_for_interval = State()
-
-
-@dp.message_handler(commands=['start'])
+ 
+ 
+# Handlers
 async def welcome(message: types.Message):
     welcome_text = "Привет! Я бот, который поможет вам с разными действиями с ценными бумагами.\n" \
                    "Введите /help, чтобы посмотреть, чем я могу вам помочь! "
     await bot.send_message(message.chat.id, welcome_text)
-
-
-@dp.message_handler(commands=['help'])
+ 
+ 
 async def help_command(message: types.Message):
     help_text = "Доступные команды:\n\n" \
                 "/start - Начать взаимодействие💰\n" \
@@ -30,64 +31,82 @@ async def help_command(message: types.Message):
                 "/parrot - Посмотреть на попугая🦜\n" \
                 "/help - Показать это сообщение🤯"
     await message.answer(help_text)
-
-
-@dp.message_handler(commands=['actions'])
-async def show_actions_keyboard(message: types.Message):
+ 
+ 
+async def show_actions_keyboard(message: types.Message, state: FSMContext):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     buttons = ["Запросить стоимость акции", "Предсказать стоимость акции", "Поставить акцию на отслеживание"]
     keyboard.add(*buttons)
     await bot.send_message(message.chat.id, "Выберите действие:", reply_markup=keyboard)
-
-    @dp.message_handler(lambda message1: message1.text == "Поставить акцию на отслеживание")
-    async def start_tracking(message1: types.Message):
-        keyboard_stocks = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-        buttons_stocks = ["Apple", "Google", "Microsoft"]
-        keyboard_stocks.add(*buttons_stocks)
-        await bot.send_message(message.chat.id, "Какую акцию вы хотите поставить на отслеживание?",
-                               reply_markup=keyboard_stocks)
-
-        @dp.message_handler(lambda message2: message2.text in buttons_stocks)
-        async def time_tracking(message2: types.Message):
-            keyboard_for_tracking = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-            buttons_time_for_tracking = ["30 минут", "2 часа", "Сутки"]
-            keyboard_for_tracking.add(*buttons_time_for_tracking)
-            await bot.send_message(message.chat.id,
-                                   f"Вы выбрали акцию {message2.text}. Как часто вы хотите отслеживать стоимость акции?",
-                                   reply_markup=keyboard_for_tracking)
-
-            @dp.message_handler(lambda message3: message3.text in buttons_time_for_tracking)
-            async def process_interval(message3: types.Message):
-                response_text = f"Акция {message2.text} поставлена на отслеживание с интервалом {message3.text}."
-                await bot.send_message(message.chat.id, response_text)
-
-
-@dp.message_handler(lambda message: message.text == "Запросить стоимость акции")
+ 
+ 
+async def start_tracking(message: types.Message):
+    response_text = f"Акция {message.text} была поставлена на отслеживание."
+    await bot.send_message(message.chat.id, response_text)
+ 
+ 
 async def stock_price_request(message: types.Message):
-    response_text = "Стоймость акции: $100"
+    response_text = "Стоимость акции: $100"
     await bot.send_message(message.chat.id, response_text)
-
-
-@dp.message_handler(lambda message: message.text == "Предсказать стоимость акции")
+ 
+ 
 async def predict_stock_price(message: types.Message):
-    response_text = "Предсказанная стоймость акции: $120"
+    response_text = "Предсказанная стоимость акции: $120"
     await bot.send_message(message.chat.id, response_text)
-
-
-@dp.message_handler(commands=['parrot'])
+ 
+ 
 async def send_random_parrot(message: types.Message):
     parrot_number = random.randint(1, 16)
-    parrot_filename = f'data/parrots/parrot{parrot_number}.jpeg'
-
+    parrot_filename = f'greenery_bot-main/data/parrots/parrot{parrot_number}.jpeg'
+ 
     with open(parrot_filename, 'rb') as photo:
-        await bot.send_photo(message.chat.id, photo, caption='Случайный попугай 🦜', reply_to_message_id=message.message_id)
-
-@dp.message_handler()
+        await bot.send_photo(message.chat.id, photo, caption='Случайный попугай 🦜',
+                             reply_to_message_id=message.message_id)
+ 
+ 
 async def unknown_message(message: types.Message):
     response_text = "Мы пока не умеем отвечать на такое сообщение. \n" \
                     "Чтобы посмотреть доступные команды, напишите /help."
     await bot.send_message(message.chat.id, response_text)
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+ 
+ 
+# Functions for Yandex.Cloud
+async def register_handlers(dp: Dispatcher):
+    """Registration all handlers before processing update."""
+    dp.register_message_handler(welcome, commands=['start'])
+    dp.register_message_handler(help_command, commands=['help'])
+    dp.register_message_handler(show_actions_keyboard, commands=['actions'])
+    dp.register_message_handler(start_tracking, lambda message: message.text == "Поставить акцию на отслеживание")
+    dp.register_message_handler(stock_price_request, lambda message: message.text == "Запросить стоимость акции")
+    dp.register_message_handler(predict_stock_price, lambda message: message.text == "Предсказать стоимость акции")
+    dp.register_message_handler(send_random_parrot, commands=['parrot'])
+    dp.register_message_handler(unknown_message)
+ 
+    log.debug('Handlers are registered.')
+ 
+ 
+async def process_event(event, dp: Dispatcher):
+    """
+    Converting a Yandex.Cloud functions event to an update and
+    handling the update.
+    """
+ 
+    update = json.loads(event['body'])
+    log.debug('Update: ' + str(update))
+ 
+    Bot.set_current(dp.bot)
+    update = types.Update.to_object(update)
+    await dp.process_update(update)
+ 
+ 
+async def handler(event, context):
+    if event['httpMethod'] == 'POST':
+        # Bot and dispatcher initialization
+        bot = Bot(token=API_TOKEN)
+        dp = Dispatcher(bot)
+ 
+        await register_handlers(dp)
+        await process_event(event, dp)
+ 
+        return {'statusCode': 200, 'body': 'ok'}
+    return {'statusCode': 200, 'body': 'ok'}
