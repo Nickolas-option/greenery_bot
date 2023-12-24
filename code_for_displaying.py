71776c4192a0ef1718beebb46f0bc5880eea9126
KirillTroshkin <129896942+KirillTroshkin@users.noreply.github.com>
2023-12-24 20:14:25 +0300

    Online dates code_for_displaying.py

--- code_for_displaying.py
+++ code_for_displaying.py
@@ -1,13 +1,17 @@
 import asyncio
 import aioschedule
 import pandas as pd
+import yahoo_fin.stock_info as si
+from datetime import datetime, timedelta
 
 
 def display_stranges(user_id, current_price):
     df = pd.read_csv(f"{user_id}.csv")
     for i in range(1, len(df)):
         if df.iloc[i, 1] == 1:
-            df2 = pd.read_csv(f"history_{df.iloc[i, 0]}.csv")
+            end_date = datetime.now()
+            start_date = end_date - timedelta(days=10)
+            df2 = si.get_data(df.iloc[i, 0], start_date, end_date)
             last_row = df2.iloc[-1]
             price = last_row[1]
             if current_price > (price * 1.05):
@@ -25,6 +29,8 @@ async def scheduler():
 
 async def on_startup(_):
     asyncio.create_task(scheduler())
+
+dp = Dispatcher(bot)
 
 dp = Dispatcher(bot)
 
