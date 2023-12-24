ff8724810355d6b83ed85dd0f216116c44b07803
KirillTroshkin <129896942+KirillTroshkin@users.noreply.github.com>
2023-12-24 17:50:30 +0300

    Add files via upload

--- code_for_displaying.py
+++ code_for_displaying.py
@@ -1,11 +1,22 @@
+import schedule
+import time
 import pandas as pd
 
-def get_last_price(csv_file, current_price):
-    df = pd.read_csv(csv_file)
+
+def job(stock, current_price):
+    df = pd.read_csv(f"history_{stock}.csv")
     last_row = df.iloc[-1]
     price = last_row[1]
     if current_price > (price * 1.05):
-        print("Куда бежишь, э")
+        print("Цена возросла больше чем на 5%!")
     if current_price < (price * 0.95):
-        print("Я в лужу упал, и в меня стреляли")
+        print("Цена упала!")
     return price
+
+
+schedule.every().day.at("12:00").do(job)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
